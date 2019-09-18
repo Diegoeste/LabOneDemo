182acd93da2325e1565b4d9eb02c4226feb37844
Diegoeste <55504249+Diegoeste@users.noreply.github.com>
2019-09-18 14:32:06 -0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+//
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Context context = getApplicationContext();
+                CharSequence text = "Hello, this is my first Android APP!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();          }
+        });
+
+
+    }
+}
